729550ab4d7c9472d62c5b2a13d4c5d795ffaac8
syyePhenomenol <yye3028@gmail.com>
2025-11-23 13:56:44 +0100

    Properly implement enum flags

--- Benchwarp/Doors/Obstacles/ObstacleSeverity.cs
+++ Benchwarp/Doors/Obstacles/ObstacleSeverity.cs
@@ -6,25 +6,25 @@ public enum ObstacleSeverity
     /// <summary>
     /// An obstacle that modifies save data on entry.
     /// </summary>
-    ModifiesSaveData,
+    ModifiesSaveData = 1 << 0,
     /// <summary>
     /// An obstacle which prevents Hornet from entering the entire room in a normal fashion.
     /// </summary>
-    InterruptsEntry,
+    InterruptsEntry = 1 << 1,
     /// <summary>
     /// An obstacle which prevents Hornet from reaching a visible position in the room.
     /// </summary>
-    LimitsRoomAccess,
+    LimitsRoomAccess = 1 << 2,
     /// <summary>
     /// An obstacle which prevents Hornet from leaving the scene via some transition that should normally be accessible.
     /// </summary>
-    LimitsExitAccess,
+    LimitsExitAccess = 1 << 3,
     /// <summary>
     /// An obstacle which prevents seeing Hornet normally, such as a mask. Does not include foreground layers which exist ordinarily.
     /// </summary>
-    LimitsVisibility,
+    LimitsVisibility = 1 << 4,
     /// <summary>
     /// An obstacle which has visually abnormal features that don't prevent seeing Hornet.
     /// </summary>
-    AbnormalVisual,
+    AbnormalVisual = 1 << 5
 }
